99575c9ea7601137e5487c979ca876c36d9c53a9
Tyler Bainbridge <tyler_bainbridge@student.uml.edu>
2016-03-10 20:16:31 -0500

    Added racket file for grading.

---
+++ exploration1.rkt
@@ -0,0 +1,16 @@
+#lang slideshow
+
+(slide
+ #:title (bt "Slideshow Thing")
+ (item (tt "First slide I think"))
+ 'next
+ (item (tt "Second slide I hope"))
+ 'next
+ (item (tt "Third slide")))
+(slide
+ #:title (tt "Another Slide Thing")
+ (item (tt "Cool beans"))
+ 'next
+ (item (tt "lambda"))
+ 'next
+ (item (tt "wow")))
