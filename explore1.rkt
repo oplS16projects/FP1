01684e76b91420d14cc671d8dcd2b3175f666aac
jwerardi <jwerardi@gmail.com>
2016-03-11 18:58:39 -0500

    Create explore1.rkt

---
+++ explore1.rkt
@@ -0,0 +1,39 @@
+#lang racket
+
+
+(require graphics/turtles)
+(require graphics/turtle-examples)
+
+
+(turtles #t)
+
+;one of these looks cool, adding a second breaks it
+;(spokes)
+;(spokes)
+
+;;very cool little design with these three
+(spyro-gyra)
+(neato)
+(graphics-bexam)
+
+
+;lame
+;(peano 15)
+
+;basic polygon
+;(regular-poly 8 190)
+
+
+;not 100% sure what these are supposed to do
+;(radial-turtles 15)
+;(spaced-turtles 15)
+;these ferns are pretty sweet
+;(fern1 15)
+;(fern2 150)
+;should turn fern2 a bit before you make it
+;just so it doesn't go off the screen
+;(turn/radians (- (/ pi 2)))
+;(fern2 50)
+
+
+
